9c9efea365ecae1e30d05795d3793dff26375a2f
tetsu.O <tententen1624@yahoo.co.jp>
2018-04-18 14:54:55 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   
   def index
     @tasks = Task.all.page(params[:page])
@@ -13,14 +14,15 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Taskを正常に作成しました。'
-      redirect_to @task
+      redirect_to root_url
     else
+      @task = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Taskが正常に作成されませんでした。'
-      render :new
+      render 'toppages/index'
     end
   end
 
@@ -40,19 +42,22 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    
     flash[:success] = 'Taskは正常に削除されました。'
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
   
   private
   
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
